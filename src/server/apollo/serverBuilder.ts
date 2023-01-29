beb3e284dc12848fcbf394da915f1c097288f65e
Sandro Richi <email@sandrorichi.de>
2023-01-29 14:11:19 +0100

    chore: update apollo server dependecies

--- src/server/apollo/serverBuilder.ts
+++ src/server/apollo/serverBuilder.ts
@@ -1,7 +1,6 @@
-import { ApolloServer } from '@apollo/server';
+import { ApolloServer, ApolloServerPlugin } from '@apollo/server';
 import type { ContextValue } from '../configuration/contextFactory';
 import { resolvers, typeDefs } from '../../models';
-import type { ApolloServerPlugin } from '@apollo/server/src/externalTypes/plugins';
 
 export const build = (plugins: ApolloServerPlugin<ContextValue>[] = []) =>
   new ApolloServer<ContextValue>({
